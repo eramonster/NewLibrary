a7666331e2f6af8a3787111df63bbd9c31791a35
kota-okano <kota.okano@proaxia-consulting.co.jp>
2025-04-17 15:42:02 +0900

    おねがい

---
+++ NewWrapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewWrapper
+//
+//  Created by 岡野皓太 on 2025/04/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewWrapper/NewWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewWrapperApp.swift
+//  NewWrapper
+//
+//  Created by 岡野皓太 on 2025/04/17.
+//
+
+import SwiftUI
+
+@main
+struct NewWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
